a6b86eba1c2a8f30567815345e18bc785a88f820
User <mail@example.com>
2023-05-18 10:54:31 +0200

    include guards, licensing

--- include/wmipp/wmipp.hxx
+++ include/wmipp/wmipp.hxx
@@ -1,4 +1,43 @@
-#pragma once
+/**
+ * WMI++ takes away the pain from interfacing with
+ * the Windows Management Instrumentation in C++.
+ *
+ * Author:				sonodima
+ * Repo:					https://github.com/sonodima/wmipp
+ * C++ Version:		20>
+ * Supported OS:  Windows
+ *
+ * ---------------------------------------------------
+ *
+ * Copyright © 2023 sonodima
+ *
+ * Permission is hereby granted, free of charge, to
+ * any person obtaining a copy of this software and
+ * associated documentation files (the “Software”),
+ * to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the
+ * following conditions:
+ *
+ * The above copyright notice and this permission notice
+ * shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF
+ * ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+ * SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+ * OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef SD_WMIPP_HXX
+#define SD_WMIPP_HXX
 
 #include <optional>
 #include <stdexcept>
@@ -309,4 +348,6 @@ namespace wmipp
 		CComPtr<IWbemLocator> locator_;
 		CComPtr<IWbemServices> services_;
 	};
-}
+} // namespace wmipp
+
+#endif // SD_WMIPP_HXX
